e608f08b1ddd4e63368e708acfd649999d2764ce
Léo Gamelin <leogamelin59850@gmail.com>
2023-09-18 12:00:07 +0200

    premier commit

---
+++ routes/book.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const router = express.Router();
+// const Ctrl = require('../controllers/book');
+// const auth = require('../middleware/auth');
+// const multer = require('../middleware/multer-config');
+// const { convertToWebp } = require('../middleware/multer-config');
+
+// router.post('/', auth, multer, convertToWebp, Ctrl.createBook);
+// router.post('/:id/rating', auth, Ctrl.addRating);
+// router.put('/:id', auth, multer, convertToWebp, Ctrl.modifyBook);
+// router.delete('/:id', auth, Ctrl.deleteBook);
+// router.get('/bestrating', Ctrl.getBest);
+// router.get('/:id', Ctrl.getOneBook);
+// router.get('/', Ctrl.getAll);
+
+// module.exports = router;
